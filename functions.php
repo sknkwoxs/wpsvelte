e4a67a52b49252659c6a989b6b6df93c15792d57
JS <js@skunkworks.co.kr>
2024-11-27 12:00:58 +0900

    Functions.php 설정

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * WPSVELTE functions and definitions
+ */
+
+// Theme setup
+require_once get_template_directory() . '/inc/register.php';
+// Timber setup
+require_once get_template_directory() . '/inc/timber.php';
+// ACF setup
+require_once get_template_directory() . '/inc/acf.php';
+// Development setup
+require_once get_template_directory() . '/inc/dev.php';
